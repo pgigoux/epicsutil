ac6c10fd5c84653c1e45cf9d3ba226daf988e7b6
pgigoux <pgigoux@gemini.edu>
2019-10-20 17:14:24 -0300

    No longer needed in Python 3.3+

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from db import EpicsRecord, DatabaseFile, EpicsDatabase, EpicsMacro
-from db import format_record_start, format_record_end, format_field
-from db import FILTER_RECORD, FILTER_FIELD
-from sch import SchematicFiles
-from files import process_file_list, list_directories
