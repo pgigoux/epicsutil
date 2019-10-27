e66f6eae16d2558eba0645dd2081cbfdfd56fea6
pgigoux <pgigoux@gemini.edu>
2019-10-27 19:09:29 -0300

    Completed EpicsMacro tests

--- test/test_macros.py
+++ test/test_macros.py
@@ -1,4 +1,3 @@
-import pytest
 from db import EpicsMacro
 
 
@@ -7,17 +6,14 @@ def test_constructor():
         EpicsMacro()
     except TypeError:
         pass
-
     try:
         EpicsMacro(None)
     except TypeError:
         pass
-
     try:
         EpicsMacro(['a', 'b', 'c'])
     except IndexError:
         pass
-
     EpicsMacro([('a', 'b'), ('c', 'd')])
 
 
@@ -27,14 +23,26 @@ def test_cleaned_macro():
 
 
 def test_get_macros():
-    m = EpicsMacro([('top', 'ccs'), ('dev', 'motor'), ('sys', 'focus')])
+    m = EpicsMacro([('top', 'cs'), ('dev', 'motor'), ('sys', 'focus')])
     assert (m.get_macros('$(top) ${top}')) == {'top'}
     assert (m.get_macros('$(top) ${dev}')) == {'top', 'dev'}
     assert (m.get_macros('$(top) ${dev} $(sys)')) == {'top', 'dev', 'sys'}
 
 
 def test_replace_macros():
-    m = EpicsMacro([('top', 'ccs'), ('dev', 'motor'), ('sys', 'focus')])
-    assert (m.replace_macros('top=$(top),dev=$(dev) ${sys}') == 'top=ccs,dev=motor focus')
-    assert (m.replace_macros('${top}$(top)$(dev)') == 'ccsccsmotor')
+    m = EpicsMacro([('top', 'cs'), ('dev', 'motor'), ('sys', 'focus')])
+    assert (m.replace_macros('top=$(top),dev=$(dev) ${sys}') == 'top=cs,dev=motor focus')
+    assert (m.replace_macros('${top}$(top)$(dev)') == 'cscsmotor')
+    try:
+        assert (m.replace_macros('${top}:$(dev):$(other)', report_undefined=True) == 'cs:motor')
+    except KeyError:
+        pass
+    assert (m.replace_macros('${top}:$(dev):$(other)', report_undefined=False) == 'cs:motor:$(other)')
 
+    m = EpicsMacro([('top', 'cs'), ('dev', 'motor'), ('sys', 'focus')], add_undefined=True)
+    assert (m.replace_macros('$(top,undefined):${dev}:$(sys):${top}') == 'cs:motor:focus:cs')
+    assert (m.replace_macros('$(top,undefined):${dev,undefined}:${sys,undefined}:${top}') == 'cs:motor:focus:cs')
+
+
+if __name__ == '__main__':
+    pass
