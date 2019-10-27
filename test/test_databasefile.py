004f7e8d729292f1c163224fbee1fa2ab7cbeb57
pgigoux <pgigoux@gemini.edu>
2019-10-27 20:11:25 -0300

    Added test for DataBaseFile class and a simple database file

---
+++ test/test_databasefile.py
@@ -0,0 +1,33 @@
+from db import DatabaseFile
+
+
+def test_constructor():
+    try:
+        DatabaseFile()
+    except FileNotFoundError:
+        pass
+    try:
+        DatabaseFile(file_name='idonotexist.db')
+    except FileNotFoundError:
+        pass
+    try:
+        DatabaseFile(file_name='idonotexist.db')
+    except FileNotFoundError:
+        pass
+
+
+def test_extract_name_and_type():
+    df = DatabaseFile(file_name='simple.db')
+    assert (df._extract_record_name_and_type('record(bi,"cs:health") {') == ('cs:health', 'bi'))
+    assert (df._extract_record_name_and_type('record(bi,"") {') == (None, None))
+    assert (df._extract_record_name_and_type('record(bi,) {') == (None, None))
+    assert (df._extract_record_name_and_type('record(,"name") {') == (None, None))
+    assert (df._extract_record_name_and_type('record(,) {') == (None, None))
+    assert (df._extract_record_name_and_type('record() {') == (None, None))
+    assert (df._extract_record_name_and_type('record(') == (None, None))
+    assert (df._extract_record_name_and_type('field(SCAN,"I/O Intr")) {') == (None, None))
+    assert (df._extract_record_name_and_type('anything else') == (None, None))
+
+
+if __name__ == '__main__':
+    test_extract_name_and_type()
