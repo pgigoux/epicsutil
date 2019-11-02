dad8ab7e5b1fdb43c2182771426bb5ebfc715b55
pgigoux <pgigoux@gemini.edu>
2019-11-02 15:38:37 -0300

    Added all tests except for the write functions

--- test/test_record.py
+++ test/test_record.py
@@ -4,14 +4,18 @@ from db import DatabaseFile, EpicsRecord
 
 
 @pytest.fixture
-def database_file():
+def epics_record():
     """
-    Fixture used to return a valid DatabaseFile object
-    :return: database file
-    :rtype: DatabaseFile
+    Fixture used to return a valid EpicsRecord object from single.db
+    :return: epics record
+    :rtype: EpicsRecord
     """
     file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'db', 'single.db')
-    return DatabaseFile(file_name=file_name)
+    df = DatabaseFile(file_name=file_name)
+    record = None
+    for record in df.next_record():
+        break
+    return record
 
 
 def test_constructor():
@@ -39,59 +43,100 @@ def test_constructor():
 
 
 def test_get_name_and_type():
-    record = EpicsRecord('name', 'type')
-    assert (record.get_name() == 'name')
-    assert (record.get_type() == 'type')
+    record = EpicsRecord('my_name', 'my_type')
+    assert (record.get_name() == 'my_name')
+    assert (record.get_type() == 'my_type')
 
 
-def test_get_field_names(database_file):
-    for record in database_file.next_record():
-        break
-    assert (isinstance(record, EpicsRecord))
-    assert (sorted(record.get_field_names()) == ['ADEL', 'AOFF', 'ASLO', 'DESC', 'DISS', 'DISV', 'DTYP', 'EGU',
-                                                 'EGUF', 'EGUL', 'EVNT', 'FLNK', 'HHSV', 'HIGH', 'HIHI', 'HOPR',
-                                                 'HSV', 'HYST', 'INP', 'LINR', 'LLSV', 'LOLO', 'LOPR', 'LOW',
-                                                 'LSV', 'MDEL', 'PHAS', 'PINI', 'PREC', 'PRIO', 'SCAN', 'SDIS',
-                                                 'SIML', 'SIMS', 'SIOL', 'SMOO'])
+def test_get_field_names_1():
+    record = EpicsRecord('my_name', 'my_type')
+    assert (record.get_field_names() == [])
 
 
-def test_get_field_value(database_file):
-    for record in database_file.next_record():
-        break
-    assert (isinstance(record, EpicsRecord))
-    assert (record.get_field_value('DESC') == 'analog input record')
-    assert (record.get_field_value('SCAN') == 'I/O Intr')
-    assert (record.get_field_value('PINI') == 'NO')
-    assert (record.get_field_value('PHAS') == '0')
-    assert (record.get_field_value('EVNT') == '0')
-    assert (record.get_field_value('DTYP') == 'Analog I/O')
-    assert (record.get_field_value('DISV') == '1')
-    assert (record.get_field_value('SDIS') == '0.000000000000000e+00')
-    assert (record.get_field_value('DISS') == 'NO_ALARM')
-    assert (record.get_field_value('PRIO') == 'LOW')
-    assert (record.get_field_value('FLNK') == '0.000000000000000e+00')
-    assert (record.get_field_value('INP') == '@memory')
-    assert (record.get_field_value('PREC') == '0')
-    assert (record.get_field_value('LINR') == 'LINEAR')
-    assert (record.get_field_value('EGUF') == '0.0000000e+00')
-    assert (record.get_field_value('EGUL') == '0.0000000e+00')
-    assert (record.get_field_value('EGU') == 'Percentage')
-    assert (record.get_field_value('HOPR') == '0.0')
-    assert (record.get_field_value('LOPR') == '0.0')
-    assert (record.get_field_value('AOFF') == '0.0000000e+00')
-    assert (record.get_field_value('ASLO') == '1.0')
-    assert (record.get_field_value('SMOO') == '0.0000000e+00')
-    assert (record.get_field_value('HIHI') == '90.0')
-    assert (record.get_field_value('LOLO') == '0.0')
-    assert (record.get_field_value('HIGH') == '60.0')
-    assert (record.get_field_value('LOW') == '0.0')
-    assert (record.get_field_value('HHSV') == 'MAJOR')
-    assert (record.get_field_value('LLSV') == 'NO_ALARM')
-    assert (record.get_field_value('HSV') == 'MINOR')
-    assert (record.get_field_value('LSV') == 'NO_ALARM')
-    assert (record.get_field_value('HYST') == '0.000000000000000e+00')
-    assert (record.get_field_value('ADEL') == '0.000000000000000e+00')
-    assert (record.get_field_value('MDEL') == '0.000000000000000e+00')
-    assert (record.get_field_value('SIOL') == '0.000000000000000e+00')
-    assert (record.get_field_value('SIML') == '0.000000000000000e+00')
-    assert (record.get_field_value('SIMS') == 'NO_ALARM')
+def test_get_field_names_2(epics_record):
+    assert (isinstance(epics_record, EpicsRecord))
+    assert (sorted(epics_record.get_field_names()) == ['ADEL', 'AOFF', 'ASLO', 'DESC', 'DISS', 'DISV', 'DTYP', 'EGU',
+                                                       'EGUF', 'EGUL', 'EVNT', 'FLNK', 'HHSV', 'HIGH', 'HIHI', 'HOPR',
+                                                       'HSV', 'HYST', 'INP', 'LINR', 'LLSV', 'LOLO', 'LOPR', 'LOW',
+                                                       'LSV', 'MDEL', 'PHAS', 'PINI', 'PREC', 'PRIO', 'SCAN', 'SDIS',
+                                                       'SIML', 'SIMS', 'SIOL', 'SMOO'])
+
+
+def test_get_field_value_1():
+    record = EpicsRecord('my_name', 'my_type')
+    assert (record.get_field_value('WHATEVER') is None)
+
+
+def test_get_field_value_2(epics_record):
+    assert (isinstance(epics_record, EpicsRecord))
+    assert (epics_record.get_field_value('DESC') == 'analog input record')
+    assert (epics_record.get_field_value('SCAN') == 'I/O Intr')
+    assert (epics_record.get_field_value('PINI') == 'NO')
+    assert (epics_record.get_field_value('PHAS') == '0')
+    assert (epics_record.get_field_value('EVNT') == '0')
+    assert (epics_record.get_field_value('DTYP') == 'Analog I/O')
+    assert (epics_record.get_field_value('DISV') == '1')
+    assert (epics_record.get_field_value('SDIS') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('DISS') == 'NO_ALARM')
+    assert (epics_record.get_field_value('PRIO') == 'LOW')
+    assert (epics_record.get_field_value('FLNK') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('INP') == '@memory')
+    assert (epics_record.get_field_value('PREC') == '0')
+    assert (epics_record.get_field_value('LINR') == 'LINEAR')
+    assert (epics_record.get_field_value('EGUF') == '0.0000000e+00')
+    assert (epics_record.get_field_value('EGUL') == '0.0000000e+00')
+    assert (epics_record.get_field_value('EGU') == 'Percentage')
+    assert (epics_record.get_field_value('HOPR') == '0.0')
+    assert (epics_record.get_field_value('LOPR') == '0.0')
+    assert (epics_record.get_field_value('AOFF') == '0.0000000e+00')
+    assert (epics_record.get_field_value('ASLO') == '1.0')
+    assert (epics_record.get_field_value('SMOO') == '0.0000000e+00')
+    assert (epics_record.get_field_value('HIHI') == '90.0')
+    assert (epics_record.get_field_value('LOLO') == '0.0')
+    assert (epics_record.get_field_value('HIGH') == '60.0')
+    assert (epics_record.get_field_value('LOW') == '0.0')
+    assert (epics_record.get_field_value('HHSV') == 'MAJOR')
+    assert (epics_record.get_field_value('LLSV') == 'NO_ALARM')
+    assert (epics_record.get_field_value('HSV') == 'MINOR')
+    assert (epics_record.get_field_value('LSV') == 'NO_ALARM')
+    assert (epics_record.get_field_value('HYST') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('ADEL') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('MDEL') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('SIOL') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('SIML') == '0.000000000000000e+00')
+    assert (epics_record.get_field_value('SIMS') == 'NO_ALARM')
+    assert (epics_record.get_field_value('WHATEVER') is None)
+
+
+def test_get_fields_1():
+    record = EpicsRecord('my_name', 'my_type')
+    assert(record.get_fields() == [])
+
+
+def test_get_fields_2(epics_record):
+    assert (isinstance(epics_record, EpicsRecord))
+    assert (sorted(epics_record.get_fields()) == [('ADEL', '0.000000000000000e+00'), ('AOFF', '0.0000000e+00'),
+                                                  ('ASLO', '1.0'), ('DESC', 'analog input record'),
+                                                  ('DISS', 'NO_ALARM'), ('DISV', '1'), ('DTYP', 'Analog I/O'),
+                                                  ('EGU', 'Percentage'), ('EGUF', '0.0000000e+00'),
+                                                  ('EGUL', '0.0000000e+00'), ('EVNT', '0'),
+                                                  ('FLNK', '0.000000000000000e+00'), ('HHSV', 'MAJOR'),
+                                                  ('HIGH', '60.0'), ('HIHI', '90.0'), ('HOPR', '0.0'), ('HSV', 'MINOR'),
+                                                  ('HYST', '0.000000000000000e+00'), ('INP', '@memory'),
+                                                  ('LINR', 'LINEAR'), ('LLSV', 'NO_ALARM'), ('LOLO', '0.0'),
+                                                  ('LOPR', '0.0'), ('LOW', '0.0'), ('LSV', 'NO_ALARM'),
+                                                  ('MDEL', '0.000000000000000e+00'), ('PHAS', '0'), ('PINI', 'NO'),
+                                                  ('PREC', '0'), ('PRIO', 'LOW'), ('SCAN', 'I/O Intr'),
+                                                  ('SDIS', '0.000000000000000e+00'), ('SIML', '0.000000000000000e+00'),
+                                                  ('SIMS', 'NO_ALARM'), ('SIOL', '0.000000000000000e+00'),
+                                                  ('SMOO', '0.0000000e+00')])
+
+
+def test_add_field():
+    record = EpicsRecord('my_name', 'my_type')
+    record.add_field('field1', 'value1')
+    record.add_field('field2', 'value2')
+    assert (record.get_field_names() == ['field1', 'field2'])
+    assert (record.get_field_value('field1') == 'value1')
+    assert (record.get_field_value('field2') == 'value2')
+    assert (record.get_fields() == [('field1', 'value1'), ('field2', 'value2')])
